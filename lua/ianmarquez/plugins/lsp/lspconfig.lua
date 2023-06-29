69d67961b602eabc98b7dc75e2adb0c2980fa78b
Ian Marquez <regilmarquez123@gmail.com>
2023-06-29 20:56:34 +0800

    removed and fixed linting issue with typescript

--- lua/ianmarquez/plugins/lsp/lspconfig.lua
+++ lua/ianmarquez/plugins/lsp/lspconfig.lua
@@ -44,7 +44,14 @@ local on_attach = function(client, bufnr)
 	end
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+-- Change the Diagnostic symbols in the sign column (gutter)
+local signs = { Error = "", Warn = "", Hint = "ﴞ", Info = "" }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
 
 -- configure html server
 lspconfig["html"].setup({
@@ -93,18 +100,6 @@ lspconfig["sqlls"].setup({
 
 -- go language server
 lspconfig["gopls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- eslint language server
-lspconfig["eslint"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- JSON language server
-lspconfig["jsonls"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 })
