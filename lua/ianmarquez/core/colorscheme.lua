1dcb6f86ab06b7fdee53c02b0b508559e092132f
Ian Marquez <regilmarquez123@gmail.com>
2023-06-30 18:27:49 +0800

    Added barbar bindings

--- lua/ianmarquez/core/colorscheme.lua
+++ lua/ianmarquez/core/colorscheme.lua
@@ -1,5 +1,7 @@
-local status, _ = pcall(vim.cmd, "colorscheme dracula")
+local status, _ = pcall(vim.cmd, "colorscheme dracula") -- dracula
+-- local status, _ = pcall(vim.cmd, "colorscheme kanagawa") -- kanagawa
+
 if not status then
-  print("Colorscheme not found!")
-  return
+	print("Colorscheme not found!")
+	return
 end
--- lua/ianmarquez/core/keymaps.lua
+++ lua/ianmarquez/core/keymaps.lua
@@ -13,16 +13,15 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+-- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
+-- keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+-- keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+-- keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+--
+-- keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- vim-maximizer
 
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- vim-maximizer
-
--- nvim-tree
+-- vim file explorer
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
@@ -30,3 +29,9 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+keymap.set("n", "<leader>to", "<cmd>BufferOrderByDirectory<cr>")
+keymap.set("n", "<leader>th", "<cmd>BufferPrevious<cr>")
+keymap.set("n", "<leader>tl", "<cmd>BufferNext<cr>")
+keymap.set("n", "<leader>tx", "<cmd>BufferClose<cr>")
+keymap.set("n", "<leader>tc", "<cmd>BufferCloseAllButCurrent<cr>")
