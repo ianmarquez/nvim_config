60ba5eecfd66ebe239cee7dfee96f63d021d5530
Ian Marquez <regilmarquez123@gmail.com>
2023-07-02 18:15:30 +0800

    Changed barbar bindings and added barbar setup table

--- lua/ianmarquez/plugins/barbar.lua
+++ lua/ianmarquez/plugins/barbar.lua
@@ -1,8 +1,19 @@
+local barbar_setup, barbar = pcall(require, "barbar")
+if not barbar_setup then
+	return
+end
+
+barbar.setup({
+	highlight_visible = true,
+	maximum_padding = 2,
+	minimum_padding = 2,
+})
+
 local keymap = vim.keymap -- barbar
 
 keymap.set("n", "<leader>to", "<cmd>BufferOrderByDirectory<cr>")
-keymap.set("n", "<leader>th", "<cmd>BufferPrevious<cr>")
-keymap.set("n", "<leader>tl", "<cmd>BufferNext<cr>")
-keymap.set("n", "<leader>tx", "<cmd>BufferClose<cr>")
+keymap.set("n", "<Tab>", "<cmd>BufferNext<cr>")
+keymap.set("n", "<S-Tab>", "<cmd>BufferPrev<cr>")
+keymap.set("n", "<C-x>", "<cmd>BufferClose<cr>")
 keymap.set("n", "<leader>tc", "<cmd>BufferCloseAllButCurrent<cr>")
 keymap.set("n", "<leader>txa", "<cmd>BufferWipeout<cr>")
