39f599a145e9f4d364914196e3df50bfb676a683
Ian Marquez <regilmarquez123@gmail.com>
2023-07-02 18:15:14 +0800

    Added floaterm config

--- lua/ianmarquez/plugins/float-term.lua
+++ lua/ianmarquez/plugins/float-term.lua
@@ -1,5 +1,5 @@
 vim.g.floaterm_keymap_toggle = "<F1>"
-vim.g.floaterm_width = 0.8
+vim.g.floaterm_width = 0.9
 vim.g.floaterm_height = 0.8
 vim.g.floatterm_wintitle = 0
 vim.g.floaterm_autoclose = 1
