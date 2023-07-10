7bbc42dd49d8fc6ca8688fbc245dab2d1d45e3d1
Ian Marquez <ian.regil@aleph-labs.com>
2023-07-10 13:48:46 +0800

    Changed foreground collor for nvim lines to be more visible

--- lua/ianmarquez/core/colorscheme.lua
+++ lua/ianmarquez/core/colorscheme.lua
@@ -1,5 +1,8 @@
 -- local status, _ = pcall(vim.cmd, "colorscheme dracula") -- dracula
+
 local status, _ = pcall(vim.cmd, "colorscheme tokyonight")
+vim.cmd("hi LineNr guifg=#f8f8f2")
+vim.cmd("hi CursorLineNr guifg=#00ffd2")
 
 if not status then
 	print("Colorscheme not found!")
