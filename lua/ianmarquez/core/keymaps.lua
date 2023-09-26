a9643a2217ba7b7813562a8153fa33adba86b7b4
Ian Marquez <regilmarquez123@gmail.com>
2023-09-26 17:43:09 +0800

    Fix: added css language server linting warnings on unknown @rules

--- lua/ianmarquez/core/keymaps.lua
+++ lua/ianmarquez/core/keymaps.lua
@@ -2,21 +2,22 @@ vim.g.mapleader = " " -- <leader>
 
 local keymap = vim.keymap -- for conciseness
 
-vim.opt.mouse = ""
-
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move text down" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move text up" })
 
-keymap.set("i", "<C-c>", "<ESC>")
-
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "No highlight" })
 keymap.set("n", "x", '"_x"')
 keymap.set("n", "J", "mzJ`z")
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "nzzzv")
-keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace word under cursor" }
+)
 
 -- window controls
 keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
