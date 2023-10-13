85de7b19976e1163fac04927c6bac8350e118745
Ian Marquez <regilmarquez123@gmail.com>
2023-10-13 15:56:06 +0800

    Update: Changed config to use none-ls instead of null-ls
    - changed null ls to use the community fork
    - added harpoon

--- lua/ianmarquez/plugins/colorscheme.lua
+++ lua/ianmarquez/plugins/colorscheme.lua
@@ -1,26 +1,17 @@
 return {
 	{
 		"Mofiqul/dracula.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme dracula]])
+			-- vim.cmd([[colorscheme dracula]])
 		end,
 	},
 	{
-		"rebelot/kanagawa.nvim",
-		lazy = true,
+		"rose-pine/neovim",
+		name = "rose-pine",
 		config = function()
 			-- load the colorscheme here
-			-- vim.cmd([[colorscheme kanagawa-wave]])
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = true,
-		config = function()
-			-- load the colorscheme here
-			-- vim.cmd([[colorscheme tokyonight-moon]])
+			vim.cmd([[colorscheme rose-pine-moon]])
 		end,
 	},
 }
