19d534aebdc9cdcee235926dfd61a6b6a5ff6d17
Ian Marquez <regilmarquez123@gmail.com>
2024-06-01 15:41:12 +0800

    Update: Added trouble version 3

--- lua/ianmarquez/plugins/lsp/trouble.lua
+++ lua/ianmarquez/plugins/lsp/trouble.lua
@@ -1,9 +1,36 @@
 return {
 	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "<leader>ge", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
-		{ "<leader>gw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Toggle Trouble Workspace Diagnostics" },
-		{ "<leader>gr", "<cmd>TroubleToggle lsp_references<cr>", desc = "Toggle Trouble LSP References" },
+		{
+			"<leader>dd",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>dD",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>ds",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>dl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>dL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>dQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
 }
