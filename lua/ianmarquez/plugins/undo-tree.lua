3d7a0a44f33771af896fb9cc9cc39e1a556b3814
Ian Marquez <regilmarquez123@gmail.com>
2024-07-29 15:59:23 +0800

    Chore: added which key groupings for keymaps that share starting letters

--- lua/ianmarquez/plugins/undo-tree.lua
+++ lua/ianmarquez/plugins/undo-tree.lua
@@ -10,6 +10,6 @@ return {
 		},
 	},
 	keys = { -- load the plugin only when using it's keybinding:
-		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle UndeTree" },
 	},
 }
--- lua/ianmarquez/plugins/which-key.lua
+++ lua/ianmarquez/plugins/which-key.lua
@@ -5,7 +5,37 @@ return {
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
 	end,
-	opts = {
-		preset = "helix",
-	},
+	config = function()
+		local wk = require("which-key")
+		wk.setup({
+			preset = "helix",
+		})
+
+		wk.add({
+			{
+				"<leader>f",
+				group = "Telescope Find",
+			},
+			{
+				"<leader>w",
+				group = "Window Control",
+			},
+			{
+				"<leader>d",
+				group = "DiffView/Diagnostics",
+			},
+			{
+				"<leader>h",
+				group = "Harpoon",
+			},
+			{
+				"<leader>m",
+				group = "Format",
+			},
+			{
+				"<leader>t",
+				group = "Tab/Trouble Control",
+			},
+		})
+	end,
 }
